4ec94b1004a2f9c9eec010a7452bc15e7b9a9393
AA <muhammadtalalshakeel2009@gmail.com>
2024-12-20 17:27:52 +0500

    first commit

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/core/theme/app_pallete.dart';
 
 class AppTheme {
-  static final darkThemeMode = ThemeData.dark();
+  static final _border=  OutlineInputBorder(
+        borderSide: BorderSide(
+          color: AppPallete.borderColor,
+          width: 3,
+        ),
+borderRadius: BorderRadius.circular(10),
+  );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(27),
+      enabledBorder: _border,
+      focusedBorder: _border
+      
+    )
+  );
 }
--- lib/pages/Signup_pages.dart
+++ lib/pages/Signup_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/widgets/auth_field.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -11,7 +12,18 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          ),
+          AuthField(hintText: "Email")
+
+
+        ],
+      ),
     );
   }
 }
