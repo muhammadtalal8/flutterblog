5ef631eebc3784bb8f665bf909e3f8d5fa1e79d4
AA <muhammadtalalshakeel2009@gmail.com>
2024-12-24 00:20:50 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Talal Blog App Demo',
       theme: AppTheme.darkThemeMode,
-      home:  SignUpPage());
+      home:  const SignUpPage());
   }
 }
 
