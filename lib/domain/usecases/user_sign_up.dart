a48b8acc2a9a1043fb0aa1ea895fa7a4aa8f5550
AA <muhammadtalalshakeel2009@gmail.com>
2024-12-31 01:00:55 +1200

    first commit

---
+++ lib/domain/usecases/user_sign_up.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_blog/core/theme/error/failure.dart';
+import 'package:flutter_blog/core/usecase/usecase.dart';
+import 'package:flutter_blog/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignup implements UserCase<String, UserSignupParams> {
+  final AuthRepository authRepository;
+
+  UserSignup({required this.authRepository});
+  @override
+  Future<Either<Failure, String>> call(UserSignupParams params) async {
+    return authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignupParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignupParams({
+    required this.email,
+    required this.password,
+    required this.name,
+  });
+}
