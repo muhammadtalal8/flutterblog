e34c26d59e4d0f24515649134656b21da54e3579
AA <muhammadtalalshakeel2009@gmail.com>
2025-01-01 20:25:30 +0500

    first commit

--- lib/presentation/bloc/auth_bloc.dart
+++ lib/presentation/bloc/auth_bloc.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_blog/domain/usecases/user_sign_up.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  final UserSignUp _userSignUp;
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) {});
+  final UserSignup _userSignUp;
+  AuthBloc({required UserSignup userSignUp})
+      : _userSignUp = userSignUp,
+        super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp(UserSignupParams(
+        email: event.email,
+        password: event.password,
+        name: event.name,
+      ));
+      res.fold((l) => emit(AuthFailure()), (r) => null);
+    });
   }
 }
