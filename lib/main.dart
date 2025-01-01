3d25216d5db774b35855fcf26c2c0d66b84e8d7f
AA <muhammadtalalshakeel2009@gmail.com>
2025-01-01 19:48:46 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/core/secret/app_secrets.dart';
 import 'package:flutter_blog/core/theme/theme.dart';
