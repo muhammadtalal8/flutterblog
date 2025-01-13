3a74649e5632e50bb75cc7eb4346f97233dc4c4a
AA <muhammadtalalshakeel2009@gmail.com>
2025-01-13 20:16:02 +0500

    first commit

--- lib/data/datasources/repostiory/auth_repository_impl.dart
+++ lib/data/datasources/repostiory/auth_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:flutter_blog/data/datasources/auth_remote_data_source.dart';
 import 'package:flutter_blog/domain/entities/user.dart';
 import 'package:flutter_blog/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as sb;
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
@@ -42,6 +43,8 @@ class AuthRepositoryImpl implements AuthRepository {
       final user = await fn();
 
       return right(user);
+    } on sb.AuthException catch (e) {
+      return left(Failure(e.message));
     } on ServerException catch (e) {
       return left(Failure(e.message));
     }
