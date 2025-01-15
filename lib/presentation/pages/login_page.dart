0eab4f655b33a87eb033691549048986838197ec
AA <muhammadtalalshakeel2009@gmail.com>
2025-01-15 22:38:18 +0500

    first commit

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_blog/core/common/widgets/loader.dart';
 import 'package:flutter_blog/core/theme/app_pallete.dart';
+import 'package:flutter_blog/core/utils/show_snakbar.dart';
 import 'package:flutter_blog/presentation/bloc/auth_bloc.dart';
 import 'package:flutter_blog/presentation/pages/Signup_pages.dart';
 import 'package:flutter_blog/presentation/widgets/auth_field.dart';
@@ -31,60 +33,73 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Sign In",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+        child: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthFailure) {
+              showSnackBar(context, state.message);
+            }
+          },
+          builder: (context, state) {
+             if (state is AuthLoading) {
+              return const Loader();
+            }
+           
+            return Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  AuthField(
+                    hintText: "Email",
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 8),
+                  AuthField(
+                    hintText: "Password",
+                    controller: passwordController,
+                    isObscureText: true,
+                  ),
+                  const SizedBox(height: 10),
+                  AuthGradientButton(
+                    buttonText: 'Sign In',
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AuthBloc>().add(AuthLogin(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim()));
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, SignUpPage.route());
+                    },
+                    child: RichText(
+                        text: TextSpan(
+                            text: "Don't have an account? ",
+                            style: Theme.of(context).textTheme.titleMedium,
+                            children: [
+                          TextSpan(
+                            text: "Sign Up",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(
+                                    color: AppPallete.gradient2,
+                                    fontWeight: FontWeight.bold),
+                          )
+                        ])),
+                  )
+                ],
               ),
-              const SizedBox(height: 8),
-              AuthField(
-                hintText: "Email",
-                controller: emailController,
-              ),
-              const SizedBox(height: 8),
-              AuthField(
-                hintText: "Password",
-                controller: passwordController,
-                isObscureText: true,
-              ),
-              const SizedBox(height: 10),
-              AuthGradientButton(
-                buttonText: 'Sign In',
-                onPressed: () {
-                  if(formKey.currentState!.validate()) {
-                    context.read<AuthBloc>().add(AuthLogin(
-                      email: emailController.text.trim(),
-                      password: passwordController.text.trim()));
-                  }
-                },
-              ),
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context, SignUpPage.route());
-                },
-                child: RichText(
-                    text: TextSpan(
-                        text: "Don't have an account? ",
-                        style: Theme.of(context).textTheme.titleMedium,
-                        children: [
-                      TextSpan(
-                        text: "Sign Up",
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.copyWith(
-                                color: AppPallete.gradient2,
-                                fontWeight: FontWeight.bold),
-                      )
-                    ])),
-              )
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
