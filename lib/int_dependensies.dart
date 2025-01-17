4efce0c808bb1e889d73bf2bd867d9cb7e82e98c
AA <muhammadtalalshakeel2009@gmail.com>
2025-01-18 00:15:09 +0545

    first commit

--- lib/int_dependensies.dart
+++ lib/int_dependensies.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_blog/core/secret/app_secrets.dart';
 import 'package:flutter_blog/data/datasources/auth_remote_data_source.dart';
 import 'package:flutter_blog/data/datasources/repostiory/auth_repository_impl.dart';
 import 'package:flutter_blog/domain/repository/auth_repository.dart';
@@ -7,10 +6,6 @@ import 'package:flutter_blog/domain/usecases/user_sign_up.dart';
 import 'package:flutter_blog/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 final serviceLocator = GetIt.instance;
-Future<void> initDependensies() async {
-  _initAuth();
-  serviceLocator.registerLazySingleton(() => supabase.client);
-}
 void _initAuth() {
   serviceLocator.registerFactory<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(serviceLocator()),
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_blog/core/secret/app_secrets.dart';
 import 'package:flutter_blog/core/theme/theme.dart';
 import 'package:flutter_blog/int_dependensies.dart';
 import 'package:flutter_blog/presentation/bloc/auth_bloc.dart';
 import 'package:flutter_blog/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initDependensies();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(create: (_) => serviceLocator<AuthBloc>()),
