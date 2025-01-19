b8c24fc26fb7d1c7e9509ea2eecc299330121126
AA <muhammadtalalshakeel2009@gmail.com>
2025-01-20 00:30:27 +0500

    first commit

--- lib/data/datasources/auth_remote_data_source.dart
+++ lib/data/datasources/auth_remote_data_source.dart
@@ -3,6 +3,7 @@ import 'package:flutter_blog/data/models/user_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
+  Session? get currentUserSession;
   Future<UserModel> signUpWithEmailPassword({
     required String name,
     required String email,
