69345a4052b2fec0cc12cbff36050cecda89b28a
AA <muhammadtalalshakeel2009@gmail.com>
2025-02-02 00:06:19 +0600

    first commit

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_blog/features/blog/presentation/pages/add_new_blogpage.dart';
+
 class BlogPage extends StatelessWidget {
   const BlogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text('Blog App'),
-      actions: [IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.add_circled),)],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blog App'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context, AddNewBlogPage.route());
+            },
+            icon: const Icon(CupertinoIcons.add_circled),
+          ),
+        ],
       ),
     );
   }
